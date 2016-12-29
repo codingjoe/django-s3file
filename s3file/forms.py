eae01ecbf7e373a5d86814e986f5e7bfead8507a
Johannes Hoppe <info@johanneshoppe.com>
2016-12-29 16:01:19 +0100

    Remove legacy code

--- s3file/forms.py
+++ s3file/forms.py
@@ -42,8 +42,6 @@ class S3FileInput(ClearableFileInput):
         return mark_safe(output)
 
     def is_initial(self, value):
-        if django.VERSION < (1, 8):
-            return bool(value and hasattr(value, 'url'))
         return super(S3FileInput, self).is_initial(value)
 
     def value_from_datadict(self, data, files, name):
