3573934806dd51f9fc0d7ffb4e44c2fb19def203
Johannes Hoppe <info@johanneshoppe.com>
2017-11-11 14:11:31 +0100

    Switch to pbr for releases

--- setup.py
+++ setup.py
@@ -2,28 +2,6 @@
 from setuptools import setup
 
 setup(
-    name='django-s3file',
-    version='3.0.3',
-    description='A lightweight file uploader input for Django and Amazon S3',
-    author='codingjoe',
-    url='https://github.com/codingjoe/django-s3file',
-    author_email='info@johanneshoppe.com',
-    license='MIT',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development',
-        "Programming Language :: Python :: 3",
-    ],
-    packages=['s3file'],
-    include_package_data=True,
-    install_requires=[
-        'django-storages',
-        'boto3',
-    ],
+    setup_requires=['pbr'],
+    pbr=True,
 )
