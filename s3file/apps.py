5e350f20f7056018380a1314ab97aeee25297f88
Johannes Hoppe <info@johanneshoppe.com>
2019-06-16 12:22:23 +0200

    Fix test suite

--- s3file/apps.py
+++ s3file/apps.py
@@ -8,11 +8,16 @@ class S3FileConfig(AppConfig):
     def ready(self):
         from django.core.files.storage import default_storage
         from storages.backends.s3boto3 import S3Boto3Storage
+        from django import forms
+        from .forms import S3FileInputMixin
 
-        if isinstance(default_storage, S3Boto3Storage):
-            from django import forms
-            from .forms import S3FileInputMixin
+        if isinstance(default_storage, S3Boto3Storage) and \
+                S3FileInputMixin not in forms.ClearableFileInput.__bases__:
+            forms.ClearableFileInput.__bases__ = \
+                (S3FileInputMixin,) + forms.ClearableFileInput.__bases__
 
-            if S3FileInputMixin not in forms.ClearableFileInput.__bases__:
-                forms.ClearableFileInput.__bases__ = \
-                    (S3FileInputMixin,) + forms.ClearableFileInput.__bases__
+        elif S3FileInputMixin in forms.ClearableFileInput.__bases__:
+            forms.ClearableFileInput.__bases__ = tuple(
+                cls for cls in forms.ClearableFileInput.__bases__
+                if cls is not S3FileInputMixin
+            )
